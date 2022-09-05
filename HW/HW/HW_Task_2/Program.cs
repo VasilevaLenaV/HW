ff4cb4248bfa7f77989d7d8cc1bef6a8504b9abe
VasilevaLenaV <charoit@icloud.com>
2022-09-06 00:27:00 +0300

    -

---
+++ HW/HW/HW_Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int? thirdNumber(int n)
+{
+    if (n <= 99)
+        return null;
+    while (n > 999)
+        n /= 10;
+
+    return n % 10;
+}
+
+var input = Convert.ToInt32(Console.ReadLine());
+
+var number = thirdNumber(input);
+
+if (number == null)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(number);
+}
+
