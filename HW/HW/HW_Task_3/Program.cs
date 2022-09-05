5e4adadda63010b81ae49d0c7124ed6edb8128fb
VasilevaLenaV <charoit@icloud.com>
2022-09-06 00:35:17 +0300

    -

---
+++ HW/HW/HW_Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+bool isWeekand(int day)
+{
+    if (day > 5 && day <= 7)
+        return true;
+
+    return false;
+}
+
+var input = Convert.ToInt32(Console.ReadLine());
+bool result = isWeekand(input);
+Console.WriteLine(result ? "Да" : "Нет");
+
