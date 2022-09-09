d2f79b7fd15bb23c89bb2ecf87e313fd6c35bb73
VasilevaLenaV <charoit@icloud.com>
2022-09-09 22:46:10 +0300

    -

--- HW/HW_Task_2/HW_Task_2/Program.cs
+++ HW/HW_Task_2/HW_Task_2/Program.cs
@@ -1,12 +1,18 @@
 ﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-
 bool IsPalindrome(string numbersCombination)
 {
+    if (!IsNumbers(numbersCombination))
+        return false;
+
     if (numbersCombination[0] == numbersCombination[4] && numbersCombination[1] == numbersCombination[3])
         return true;
 
     return false;
+}
+bool IsNumbers(string numbersCombination)
+{
+    return numbersCombination.Length == 5 && int.TryParse(numbersCombination, out int res);
 }
 
 var input = Convert.ToString(Console.ReadLine());
